e817d05f684adbd2c4b0950a9bf3d5fc8e21c29a
András Pataky <andras.pataky@stud.uni-corvinus.hu>
2021-09-30 11:58:27 +0200

    Listbox datasource and new user button

--- UserMaintenance/UserMaintenance/Entities/User.cs
+++ UserMaintenance/UserMaintenance/Entities/User.cs
@@ -1,20 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace UserMaintenance.Entities
-{ 
-    public class User
+{
+    class User
     {
-        public Guid ID { get; set; } = Guid.NewGuid();
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string FullName {
-            get
+            public Guid ID { get; set; } = Guid.NewGuid();
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string FullName
             {
-                return string.Format(
-                    "{0} {1}",
-                    LastName,
-                    FirstName);
-            }    
-        }
+                get
+                {
+                    return string.Format(
+                        "{0} {1}",
+                        LastName,
+                        FirstName);
+                }
+            }
     }
 }
