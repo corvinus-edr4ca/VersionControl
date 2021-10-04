651341ee340bceac7496c73386c151f107c365a2
András Pataky <andras.pataky@stud.uni-corvinus.hu>
2021-10-04 13:00:55 +0200

    create list, orm and loaddata void

--- Week04_Excel_output/Week04_Excel_output/Form1.cs
+++ Week04_Excel_output/Week04_Excel_output/Form1.cs
@@ -12,9 +12,19 @@ namespace Week04_Excel_output
 {
     public partial class Form1 : Form
     {
+
+        List<Flat> Flats;
+        RealEstateEntities context = new RealEstateEntities();
+
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            Flats = context.Flat.ToList();
         }
     }
 }
