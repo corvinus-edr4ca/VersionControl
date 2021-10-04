3d90d315503e1afc7dbc49cce5c76c60c203f7fa
András Pataky <andras.pataky@stud.uni-corvinus.hu>
2021-10-04 14:19:54 +0200

    tablaformazasok

--- Week04_Excel_output/Week04_Excel_output/Form1.cs
+++ Week04_Excel_output/Week04_Excel_output/Form1.cs
@@ -18,9 +18,9 @@ namespace Week04_Excel_output
         List<Flat> Flats;
         RealEstateEntities context = new RealEstateEntities();
 
-        Excel.Application x1App;
-        Excel.Workbook x1WB;
-        Excel.Worksheet x1Sheet;
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
 
         public Form1()
         {
@@ -38,24 +38,24 @@ namespace Week04_Excel_output
         {
             try
             {
-                x1App = new Excel.Application();
-                x1WB = x1App.Workbooks.Add(Missing.Value);
-                x1Sheet = x1WB.ActiveSheet;
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWB.ActiveSheet;
 
                 CreateTable();
 
-                x1App.Visible = true;
-                x1App.UserControl = true;
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
             }
             catch (Exception ex)
             {
                 string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
                 MessageBox.Show(errMsg, "Error");
 
-                x1WB.Close(false, Type.Missing, Type.Missing);
-                x1App.Quit();
-                x1WB = null;
-                x1App = null;
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
             }
 
         }
@@ -77,7 +77,7 @@ namespace Week04_Excel_output
 
             for (int i = 0; i < headers.Length; i++)
             {
-                x1Sheet.Cells[1, i+1] = headers[i];
+                xlSheet.Cells[1, i+1] = headers[i];
             }
 
             object[,] values = new object[Flats.Count, headers.Length];
@@ -100,16 +100,37 @@ namespace Week04_Excel_output
                 values[counter, 5] = f.NumberOfRooms;
                 values[counter, 6] = f.FloorArea;
                 values[counter, 7] = f.Price;
-                values[counter, 8] = "=" + GetCell(counter, 6) + "/" + GetCell(counter, 7);
+                values[counter, 8] = "=" + GetCell(counter+2, 8) + "*1000000/" + GetCell(counter+2, 7);
                 counter++;
             }
 
-            x1Sheet.get_Range(
+            xlSheet.get_Range(
                 GetCell(2, 1),
                 GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
 
+            // Formázás
 
-                
+            Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+            headerRange.Font.Bold = true;
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 40;
+            headerRange.Interior.Color = Color.LightBlue;
+            headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+
+            int lastRowID = xlSheet.UsedRange.Rows.Count;
+            Excel.Range dataRange = xlSheet.get_Range(GetCell(2, 1), GetCell(lastRowID, headers.Length));
+            dataRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+
+            Excel.Range elsooszlop = xlSheet.get_Range(GetCell(2, 1), GetCell(lastRowID, 1));
+            elsooszlop.Font.Bold = true;
+            elsooszlop.Interior.Color = Color.LightYellow;
+
+            Excel.Range utolsooszlop = xlSheet.get_Range(GetCell(2, headers.Length), GetCell(lastRowID, headers.Length));
+            utolsooszlop.Interior.Color = Color.LightGreen;
+            utolsooszlop.NumberFormat = "0.00";
+
         }
 
         private string GetCell(int x, int y)
@@ -128,5 +149,6 @@ namespace Week04_Excel_output
 
             return ExcelCoordinate;
         }
+
     }
 }
