bf2ec195d072221e7f2512d74488bae9fe76a64d
András Pataky <andras.pataky@stud.uni-corvinus.hu>
2021-10-28 13:37:26 +0200

    ratedata entity and datagridview created

--- Week06_Web/Week06_Web/Form1.cs
+++ Week06_Web/Week06_Web/Form1.cs
@@ -7,16 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Week06_Web.Entities;
 using Week06_Web.MnbServiceReference;
 
 namespace Week06_Web
 {
     public partial class Form1 : Form
     {
+
+        BindingList<RateData> rates = new BindingList<RateData>();
+
         public Form1()
         {
             InitializeComponent();
             Fuggveny1();
+
+            dataGridView1.DataSource = rates;
         }
         public void Fuggveny1()
         {
