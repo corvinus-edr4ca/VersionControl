166d5501def4c1cdf50c71b7821c3b4db27e1dc5
András Pataky <andras.pataky@stud.uni-corvinus.hu>
2021-10-28 13:49:44 +0200

    xml text load fgv created

--- Week06_Web/Week06_Web/Form1.cs
+++ Week06_Web/Week06_Web/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using Week06_Web.Entities;
 using Week06_Web.MnbServiceReference;
 
@@ -16,15 +17,17 @@ namespace Week06_Web
     {
 
         BindingList<RateData> rates = new BindingList<RateData>();
+        string result;
 
         public Form1()
         {
             InitializeComponent();
-            Fuggveny1();
+            mnbCall();
+            getXml();
 
             dataGridView1.DataSource = rates;
         }
-        public void Fuggveny1()
+        public void mnbCall()
         {
             var mnbService = new MNBArfolyamServiceSoapClient();
             var request = new GetExchangeRatesRequestBody()
@@ -34,8 +37,31 @@ namespace Week06_Web
                 endDate = "2020-06-30"
             };
             var response = mnbService.GetExchangeRates(request);
-            var result = response.GetExchangeRatesResult;
+            result = response.GetExchangeRatesResult;
+        }
 
+        public void getXml()
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                rates.Add(rate);
+
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                {
+                    rate.Value = value / unit;
+                }
+            }
         }
     }
 }
