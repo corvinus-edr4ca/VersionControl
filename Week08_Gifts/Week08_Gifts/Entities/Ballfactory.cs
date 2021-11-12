2b4788899f70d60de2bdc715089c9466287f0cd6
András Pataky <andras.pataky@stud.uni-corvinus.hu>
2021-11-12 13:23:32 +0100

    ballfactory class created

---
+++ Week08_Gifts/Week08_Gifts/Entities/Ballfactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week08_Gifts.Entities
+{
+    class Ballfactory
+    {
+        public Ball CreateBall()
+        {
+            return new Ball();
+        }
+    }
+}
