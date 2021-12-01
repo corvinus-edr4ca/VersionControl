9c52ef014897e50fbc8d97bad9950ef5170de244
András Pataky <andras.pataky@stud.uni-corvinus.hu>
2021-12-01 19:07:25 +0100

    project done

--- Week09_MicroSimulation/Week09_MicroSimulation/Form1.Designer.cs
+++ Week09_MicroSimulation/Week09_MicroSimulation/Form1.Designer.cs
@@ -29,20 +29,130 @@ namespace Week09_MicroSimulation
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericEndYear = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textFileBrowse = new System.Windows.Forms.TextBox();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericEndYear)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(5, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Záróév:";
+            // 
+            // numericEndYear
+            // 
+            this.numericEndYear.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.numericEndYear.Location = new System.Drawing.Point(143, 13);
+            this.numericEndYear.Maximum = new decimal(new int[] {
+            2050,
+            0,
+            0,
+            0});
+            this.numericEndYear.Minimum = new decimal(new int[] {
+            2005,
+            0,
+            0,
+            0});
+            this.numericEndYear.Name = "numericEndYear";
+            this.numericEndYear.Size = new System.Drawing.Size(120, 41);
+            this.numericEndYear.TabIndex = 1;
+            this.numericEndYear.Value = new decimal(new int[] {
+            2024,
+            0,
+            0,
+            0});
+            this.numericEndYear.ValueChanged += new System.EventHandler(this.numericEndYear_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(269, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(233, 37);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Népesség fájl:";
+            // 
+            // textFileBrowse
+            // 
+            this.textFileBrowse.Enabled = false;
+            this.textFileBrowse.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textFileBrowse.Location = new System.Drawing.Point(508, 12);
+            this.textFileBrowse.Name = "textFileBrowse";
+            this.textFileBrowse.Size = new System.Drawing.Size(456, 41);
+            this.textFileBrowse.TabIndex = 3;
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonBrowse.Location = new System.Drawing.Point(970, 12);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(168, 49);
+            this.buttonBrowse.TabIndex = 4;
+            this.buttonBrowse.Text = "Browse";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            this.buttonBrowse.Click += new System.EventHandler(this.buttonBrowse_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonStart.Location = new System.Drawing.Point(1144, 11);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(168, 50);
+            this.buttonStart.TabIndex = 5;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 90);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1300, 638);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1206, 744);
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1328, 736);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.buttonBrowse);
+            this.Controls.Add(this.textFileBrowse);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericEndYear);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericEndYear)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericEndYear;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textFileBrowse;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
--- Week09_MicroSimulation/Week09_MicroSimulation/Form1.cs
+++ Week09_MicroSimulation/Week09_MicroSimulation/Form1.cs
@@ -18,33 +18,81 @@ namespace Week09_MicroSimulation
         List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
         List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
 
+        int currentYear;
+        int currentMales;
+        int currentFemales;
+
         Random rng = new Random(1234);
+
+        int maxYear = 2024;
+        string filePath;
 
         public Form1()
         {
             InitializeComponent();
-            Population = GetPopulation(@"C:\Temp\nép-teszt.csv");
+            
             BirthProbabilities = GetBirthProbabilities(@"C:\Temp\születés.csv");
             DeathProbabilities = GetDeathProbabilities(@"C:\Temp\halál.csv");
+        }
 
-            for (int year = 2005; year <= 2024; year++)
+        public void Simulation()
+        {
+            richTextBox1.Clear();
+
+            for (int year = 2005; year <= maxYear; year++)
             {
                 for (int i = 0; i < Population.Count; i++)
                 {
-                    //szimuláció
+                    SimStep(year, Population[i]);
                 }
 
-            int nbrOfMales = (from x in Population
-                              where x.Gender == Gender.Male && x.IsAlive
-                              select x).Count();
+                int nbrOfMales = (from x in Population
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+                currentMales = nbrOfMales;
 
-            int nbrOfFemales = (from x in Population
-                              where x.Gender == Gender.Female && x.IsAlive
-                              select x).Count();
+                int nbrOfFemales = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+                currentFemales = nbrOfFemales;
 
-            Console.WriteLine(string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+                currentYear = year;
+
+                DisplayResults();
             }
+        }
 
+        public void DisplayResults()
+        {
+            richTextBox1.Text += (String.Format("Szimulációs év: {0}\n\tFiúk: {1}\n\tLányok: {2}\n\n", currentYear, currentMales, currentFemales));
+        }
+
+        public void SimStep(int year, Person person)
+        {
+            if (!person.IsAlive) return;
+
+            byte age = (byte)(year - person.BirthYear);
+
+            double pDeath = (from x in DeathProbabilities
+                             where x.Gender == person.Gender && x.Age == age
+                             select x.DProbability).FirstOrDefault();
+
+            if (rng.NextDouble() <= pDeath) person.IsAlive = false;
+
+            if (person.IsAlive && person.Gender == Gender.Female)
+            {
+                double pBirth = (from x in BirthProbabilities
+                                 where x.Age == age
+                                 select x.BProbability).FirstOrDefault();
+                if (rng.NextDouble() <= pBirth)
+                {
+                    Person újszülött = new Person();
+                    újszülött.BirthYear = year;
+                    újszülött.NbrOfChildren = 0;
+                    újszülött.Gender = (Gender)(rng.Next(1, 3));
+                    Population.Add(újszülött);
+                }
+            }
         }
 
         public List<Person> GetPopulation(string csvpath)
@@ -102,6 +150,32 @@ namespace Week09_MicroSimulation
                 }
             }
             return deathProbabilities;
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            Population = GetPopulation(textFileBrowse.Text);
+            Simulation();
+        }
+
+        private void numericEndYear_ValueChanged(object sender, EventArgs e)
+        {
+            maxYear = (int)numericEndYear.Value;
+        }
+
+        private void buttonBrowse_Click(object sender, EventArgs e)
+        {
+            using(OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.InitialDirectory = @"C:\Temp";
+                ofd.Filter = "CSV (*.csv)|*csv";
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = ofd.FileName;
+                }
+            }
+            textFileBrowse.Text = filePath;
         }
     }
 }
