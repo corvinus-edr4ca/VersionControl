023ddeb264309abb59fb3026323bf169fcf4f7bb
András Pataky <andras.pataky@stud.uni-corvinus.hu>
2021-12-01 17:50:03 +0100

    for cycle for simulation and male/female count created

--- Week09_MicroSimulation/Week09_MicroSimulation/Form1.cs
+++ Week09_MicroSimulation/Week09_MicroSimulation/Form1.cs
@@ -26,6 +26,25 @@ namespace Week09_MicroSimulation
             Population = GetPopulation(@"C:\Temp\nép.csv");
             BirthProbabilities = GetBirthProbabilities(@"C:\Temp\születés.csv");
             DeathProbabilities = GetDeathProbabilities(@"C:\Temp\halál.csv");
+
+            for (int year = 2005; year <= 2024; year++)
+            {
+                for (int i = 0; i < Population.Count; i++)
+                {
+                    //szimuláció
+                }
+
+            int nbrOfMales = (from x in Population
+                              where x.Gender == Gender.Male && x.IsAlive
+                              select x).Count();
+
+            int nbrOfFemales = (from x in Population
+                              where x.Gender == Gender.Female && x.IsAlive
+                              select x).Count();
+
+            Console.WriteLine(string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfFemales, nbrOfMales));
+            }
+
         }
 
         public List<Person> GetPopulation(string csvpath)
