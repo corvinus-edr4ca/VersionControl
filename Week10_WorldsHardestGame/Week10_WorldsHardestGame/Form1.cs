fa1fcafa8a67443efaada36a7ed87660cbc3827f
András Pataky <andras.pataky@stud.uni-corvinus.hu>
2021-12-10 20:24:54 +0100

    winner chosen

--- Week10_WorldsHardestGame/Week10_WorldsHardestGame/Form1.cs
+++ Week10_WorldsHardestGame/Week10_WorldsHardestGame/Form1.cs
@@ -22,6 +22,8 @@ namespace Week10_WorldsHardestGame
         int nbrOfStepsIncrement = 10;
         int generation = 1;
 
+        Brain winnerBrain = null;
+
         public Form1()
         {
             InitializeComponent();
@@ -64,6 +66,17 @@ namespace Week10_WorldsHardestGame
                 else
                     gc.AddPlayer(b.Mutate());
             }
+
+            var winners = from p in topPerformers
+                          where p.IsWinner
+                          select p;
+            if (winners.Count() > 0)
+            {
+                winnerBrain = winners.FirstOrDefault().Brain.Clone();
+                gc.GameOver -= Gc_GameOver;
+                return;
+            }
+
             gc.Start();
         }
     }
